4e40f3544cb9626a69de291b8f14dcf5d6b23e1d
Heather Booker <heather.booker@hotmail.com>
2015-05-21 16:11:57 -0700

    buttons under canvas to add sandwich elements one by one

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 var canvas = document.getElementById('mycanvas'),
     context = canvas.getContext('2d');
 
-context.beginPath();
-context.moveTo(50, 50);
-context.lineTo(100, 300);
-//context.stroke();
+var sandwichMaker = {
+    // context.beginPath();
+    // context.moveTo(50, 50);
+    // context.lineTo(100, 300);
+    // context.stroke();
 
-//top bread
-context.fillStyle = '#B3834B';
-context.fillRect(150, 100, 200, 30);
+    addTopBread: function() {
+        context.fillStyle = '#B3834B';
+        context.fillRect(150, 100, 200, 30);
+    },
 
+    addGreens: function() {
+        context.fillStyle = "green";
+        context.fillRect(137, 130, 200, 25);
+    },
 
-//greens
-context.fillStyle = "green";
-context.fillRect(137, 130, 200, 25);
+    addCheese: function() {
+        context.fillStyle = 'yellow';
+        context.fillRect(140, 185, 220, 15);
+    },
 
-//cheese
-context.fillStyle = 'yellow';
-context.fillRect(140, 185, 220, 15);
+    addTomato: function() {
+        context.fillStyle = 'tomato';
+        context.fillRect(160, 155, 160, 30);
+    },
 
-//tomato
-context.fillStyle = 'tomato';
-context.fillRect(160, 155, 160, 30);
+    addBottomBread: function() {
+        context.fillStyle = '#B3834B';
+        context.fillRect(150, 200, 200, 30);
+    },
 
-//bottom bread
-context.fillStyle = '#B3834B';
-context.fillRect(150, 200, 200, 30);
+};
